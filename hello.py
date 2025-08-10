30f04bd194672f399ce5b7adc742b7135bae3403
Kianpey <kianpeyrovan@gmail.com>
2025-08-10 15:08:48 +0000

    a few changes

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 n=input("what is your name?").title()
 if "," in n:
     last,first=n.split(",")
-    print(f"hello, {first} {last}")
+    print(f"hello, {first.strip()} {last.strip()}")
 if "," not in n:
         print(f"hello, {n}")
