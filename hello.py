6088b19e07f9f4cf00b72e2b56a8e3b88cbf4634
Kianpey <kianpeyrovan@gmail.com>
2025-08-10 17:47:24 +0000

    asad

--- hello.py
+++
@@ -1,7 +0,0 @@
-r=input("what is your name?").title()
-if "," in r:
-    lastname,firstname=r.split(",")
-    print(f"hello, {firstname.strip()} {lastname.strip()}")
-    
-else:
-    print(f"Hello, {r}")
