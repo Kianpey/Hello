40fd5021b8c14b6d793e86b98c154c0a1df76d3f
Kianpey <kianpeyrovan@gmail.com>
2025-08-28 07:56:56 +0000

    game of random number

---
+++ number.py
@@ -0,0 +1,14 @@
+import random
+choiced=random.randint(1,100)
+print(choiced)
+while True:
+    number=int(input("number:"))
+    if number==choiced:
+        print("your choiced number is correct number")
+        break
+    elif number>choiced:
+        print("your number is big than choiced number")
+        continue
+    elif number<choiced:
+        print("the choiced number is bigger than your number")
+        continue
