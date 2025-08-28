a91b456dc9758cfcd9732644c726e3be7d84f729
Kianpey <kianpeyrovan@gmail.com>
2025-08-28 08:35:17 +0000

    hh

--- number.py
+++ number.py
@@ -3,12 +3,16 @@ choiced=random.randint(1,100)
 
 while True:
     number=int(input("number:"))
+    
     if number==choiced:
         print("your choiced number is correct number")
         break
+    
     elif number>choiced:
         print("your number is bigeer than choiced number")
         continue
+    
     elif number<choiced:
         print("the choiced number is bigger than your number")
         continue
+    
