e45962080c5e8cdf1e8c583fd92f0e34298fa519
Kianpey <kianpeyrovan@gmail.com>
2025-09-25 11:05:33 +0330

    Created using Colab

---
+++ zarb.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "eRFa550Xs0i1",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "58820beb-9ea6-4510-e4f9-fd57be2d85d8"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "4\n",
+            "4 × 0 = 0\n",
+            "4 × 1 = 4\n",
+            "4 × 2 = 8\n",
+            "4 × 3 = 12\n",
+            "4 × 4 = 16\n",
+            "4 × 5 = 20\n",
+            "4 × 6 = 24\n",
+            "4 × 7 = 28\n",
+            "4 × 8 = 32\n",
+            "4 × 9 = 36\n",
+            "4 × 10 = 40\n",
+            "hello\n"
+          ]
+        }
+      ],
+      "source": [
+        "num=int(input())\n",
+        "for i in range(0,11):\n",
+        "  print(num,\"×\",i,\"=\",num*i)\n"
+      ]
+    }
+  ]
+}
